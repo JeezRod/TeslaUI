89f9ee4c84018381083be1a4f48105e76b69645e
Rod Rivas <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2022-06-20 12:04:40 -0400

    add props to the component

--- App.js
+++ App.js
@@ -13,7 +13,23 @@ import CarItem from './src/components/CarItem/CarItem';
 const App = () => {
   return (
     <View style={styles.container}>
-      <CarItem />
+      <CarItem
+        name={'Model 3'}
+        tagLine={'Order Online for'}
+        tagLineDO={'Touchless Delivery'}
+        DOURL={'https://www.tesla.com/en_CA/support/taking-delivery'}
+        image={require('./assets/images/Model3.jpeg')}
+        button={[
+          {
+            text: 'Custom Order',
+            url: 'https://www.tesla.com/en_ca/model3/design',
+          },
+          {
+            text: 'Existing Inventory',
+            url: 'https://www.tesla.com/en_ca/inventory/new/m3',
+          },
+        ]}
+      />
     </View>
   );
 };
